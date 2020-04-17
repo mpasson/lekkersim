78c7ffeb5f4c9bf87959bd077e8183c998120b39
Marco Passoni <mpasson91@gmail.com>
2020-04-17 11:26:17 +0200

    Added solver method to Solver class

--- add_drop2.py
+++ add_drop2.py
@@ -14,15 +14,15 @@ WG2=solver.Structure(model=solver.waveguide(20.0))
 Sol=solver.Solver(structures=[BS1,BS2,WG1,WG2])
 
 
-Sol.connect(BS1,'b0',WG1,'a0')  
-Sol.connect(BS1,'b1',WG2,'a0')
-Sol.connect(BS2,'a0',WG1,'b0')
-Sol.connect(BS2,'a1',WG2,'b0')
+Sol.connect(BS1,'b1',WG1,'a0')  
+Sol.connect(BS1,'a1',WG2,'a0')
+Sol.connect(BS2,'b0',WG1,'b0')
+Sol.connect(BS2,'a0',WG2,'b0')
 
 pin_mapping={
     'a0': (BS1,'a0'),
-    'a1': (BS1,'a1'),
-    'b0': (BS2,'b0'),
+    'b0': (BS1,'b0'),
+    'a1': (BS2,'a1'),
     'b1': (BS2,'b1'),
 }
 
@@ -34,7 +34,7 @@ for Lam in np.linspace(1.4,1.6,1001):
     Sol.set_param('Lam',value=Lam)
     full=Sol.solve()
     new=solver.Structure(model=full.get_model(pin_mapping))
-    print(5*'%15.8f' % (Lam,new.get_T('a0','b0'),new.get_T('a1','b1'),new.get_T('a0','b1'),new.get_T('a1','b0')))
+    print('%15.8f %15.8f %15.8f' % (Lam,new.get_T('a0','b0'),new.get_T('a0','a1')))
 
 
 
--- solver/sol.py
+++ solver/sol.py
@@ -38,8 +38,62 @@ class Solver:
         self.connections[(structure1,pin1)]=(structure2,pin2)
         self.free_pins.remove((structure1,pin1))
         self.free_pins.remove((structure2,pin2))
+        structure1.add_conn(pin1,structure2,pin2)    
+        structure2.add_conn(pin2,structure1,pin1)    
+
 
     def show_free_pins(self):
         for st,pin in self.free_pins:
-            print ('(%50s, %5s)' % (st,pin))
+            try:
+                pinname=list(self.pin_mapping.keys())[list(self.pin_mapping.values()).index((st,pin))]
+                print ('(%50s, %5s) --> %5s' % (st,pin,pinname))
+            except AttributeError:
+                print ('(%50s, %5s)' % (st,pin))
+               
+
+    def show_pin_mapping(self):
+        try:
+            for pinname,(st,pin) in self.pin_mapping.items():
+                print ('%5s --> (%50s, %5s)' % (pinname,st,pin))
+        except AttributeError:
+            print('No mapping defined')
+            
+
+    def map_pins(self,pin_mapping):
+        self.pin_mapping=pin_mapping
+
+
+    def solve(self):
+        for st in self.structures:
+            try:
+                st.model.param_dic.update(self.param_dic)
+            except AttributeError:
+                pass
+        st_list=copy(self.structures)
+        while len(st_list)!=1:
+            source_st=st_list[0].gone_to
+            tar_st=st_list[0].connected_to[0].gone_to
+            #print('Started join step')
+            #print('Source structure: %50s inside %50s' % (source_st,source_st.gone_to))
+            #print('Target structure: %50s inside %50s' % (tar_st,tar_st.gone_to))
+            #print('\nSource connections')
+            #source_st.print_conn()
+            #print('\nTarget connections')
+            #tar_st.print_conn()
+            new_st=source_st.join(tar_st)
+            st_list.remove(source_st)
+            st_list.remove(tar_st)
+            st_list.append(new_st)
+            #print('\nGener. structure: %50s' % (new_st))
+            #print('Generated connections')
+            #new_st.print_conn()
+            #print('\n')
+        self.Final=st_list[0]
+        for st in self.structures:
+            st.reset()
+        return st_list[0] 
+
+
+    def set_param(self,name,value=None):
+        self.param_dic[name]=value
 
