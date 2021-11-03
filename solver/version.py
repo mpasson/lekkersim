ffe00b86a68714a85f12d31d86f004b987a172e8
marco <mpasson91@gmail.com>
2021-11-03 10:06:10 +0100

    Added version.py

---
+++ solver/version.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+# To modify this file you have to (also) change the make-package script.
+
+mdir = os.path.dirname(os.path.abspath(__file__))
+__version__ = subprocess.check_output(['git', 'describe'], cwd=mdir).\
+    strip().decode('utf-8') + '-dev'
+
+git_clean = subprocess.check_output(['git', 'diff-index', 'HEAD'], cwd=mdir).\
+    strip().decode('utf-8')==''
+
