cac19f9415f06ecac1a55ef8d9f7af46dc52090b
Marco Passoni <mpasson91@gmail.com>
2022-07-26 16:48:15 +0200

    Bug fix in __init__.py

--- solver/__init__.py
+++ solver/__init__.py
@@ -20,6 +20,7 @@ sol_list = []
 
 from .version import __version__, git_clean
 from solver.log import logfile, debugfile, logger
+from solver.scattering import S_matrix
 from solver.structure import Structure
 from solver.sol import *
 from solver.model import *
