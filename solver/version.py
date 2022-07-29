3fc9fb955f04a76cf3d678bf19d4fdb5d44ec71c
Marco Passoni <mpasson91@gmail.com>
2022-07-29 14:03:17 +0200

    Reworked version to handle missing git

--- solver/version.py
+++ solver/version.py
@@ -2,16 +2,30 @@ import subprocess
 import os
 
 # To modify this file you have to (also) change the make-package script.
-
 mdir = os.path.dirname(os.path.abspath(__file__))
-__version__ = (
-    subprocess.check_output(["git", "describe"], cwd=mdir).strip().decode("utf-8")
-    + "-dev"
-)
 
-git_clean = (
-    subprocess.check_output(["git", "diff-index", "HEAD"], cwd=mdir)
-    .strip()
-    .decode("utf-8")
-    == ""
-)
+try:
+    subprocess.check_output(["git", "--version"], cwd=mdir).strip().decode("utf-8")
+    git = True
+except subprocess.CalledProcessError:
+    git = False
+
+
+def get_version():
+    if not git:
+        return 'no version available', True
+    version = (
+        subprocess.check_output(["git", "describe"], cwd=mdir).strip().decode("utf-8")
+        + "-dev"
+        )
+    git_clean = (
+        subprocess.check_output(["git", "diff-index", "HEAD"], cwd=mdir)
+        .strip()
+        .decode("utf-8")
+        == ""
+    )
+    return version, git_clean
+
+
+__version__, git_clean = get_version()
+
