237d6a037a12521e29db509a439b45626dd10ac7
Marco Passoni <mpasson91@gmail.com>
2024-07-11 12:34:13 +0200

    Moved version generation in dedicated file

--- lekkersim/__init__.py
+++ lekkersim/__init__.py
@@ -11,18 +11,7 @@
 
 """ Initialization file
 """
-import os
-
-try:
-    from ._version import __version__
-except ModuleNotFoundError:
-    from hatch_vcs.version_source import VCSVersionSource
-    import toml
-
-    basedir = os.path.split(os.path.dirname(__file__))[0]
-    config = toml.load(os.path.join(basedir, "pyproject.toml"))
-    vcs_version = VCSVersionSource(basedir, config["tool"]["hatch"]["version"])
-    __version__ = vcs_version.get_version_data()["version"]
+from ._version import __version__
 
 import logging
 
@@ -31,6 +20,7 @@ logger = logging.getLogger(__name__)
 sol_list = []
 
 from .log import logfile, debugfile
+from .pin import Pin
 from .scattering import S_matrix
 from .structure import Structure
 from .sol import *
