47e5caac0c2432e7499d3edaf2a642eae77f9c9c
Marco Passoni <mpasson91@gmail.com>
2024-07-12 18:23:08 +0200

    Updated pin docstring

--- lekkersim/pin.py
+++ lekkersim/pin.py
@@ -7,11 +7,26 @@ import lekkersim as lk
 
 @dataclass(frozen=True)
 class Pin:
+    """Class representing a pin in the circuit.
+
+    Applicable to models, solvers, and structures.
+
+    It stores a base name and a mode neame. The mode name is optional and is used to distinguish between different modes of the same pin.
+    """
+
     basename: str
+    """Base name of the pin"""
+
     mode_name: Optional[str] = None
+    """Mode name of the pin"""
 
     @property
     def name(self):
+        """Full name of the pin.
+
+        - {base name}_{mode name} if mode name is not None,
+        - base name otherwise
+        """
         if self.mode_name is None:
             return self.basename
         return f"{self.basename}_{self.mode_name}"
@@ -22,11 +37,11 @@ class Pin:
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.basename!r}, {self.mode_name!r})"
 
-    def put(self, pin: Optional[tuple[lk.Structure, Self]] = None):
-        """Maps the pins in the tuple to self.name
+    def put(self, pin: Optional[lk.StructurePin] = None):
+        """Creates an pin in the solver and maps it to the given pin.
 
         Args:
-            pin (tuple) : tuple of (structure (Structure), pin (str)) containing the data to the pin to be mapped
+            pin (StructurePin) : Structure pin to be mapped.
         """
         if pin is not None:
             lk.sol_list[-1].map_pins({self: pin})
